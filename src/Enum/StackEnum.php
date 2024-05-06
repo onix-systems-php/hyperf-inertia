0a098a59db0cc12c996f9f495352d5fc2cc05fd7
harry <contact@harry.plus>
2024-05-06 10:15:10 +0530

    remove unused annotation

--- src/Enum/StackEnum.php
+++ src/Enum/StackEnum.php
@@ -1,15 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace OnixSystemsPHP\HyperfInertia\Enum;
 
-use OpenApi\Attributes as OA;
-
-#[OA\Schema(type: 'string', enum: ['vue', 'react', 'svelte'])]
 enum StackEnum: string
 {
     case VUE = 'vue';
     case REACT = 'react';
     case SVELTE = 'svelte';
 }
-
-
