d5740a08b7111f542c939212a25df98b9067fd2d
tahyhax <dmytro.bekeshenko@onix-systems.com>
2024-07-09 16:16:26 +0300

    feat(example-frontend-part): frontend part example - fix vite config - readme

--- src/Commands/Stubs/Svelte/inertia.blade.php
+++ src/Commands/Stubs/Svelte/inertia.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @php $resource = ['storage/inertia/js/app.js', "storage/inertia/js/Pages/". $page['component'] . ".svelte"]; @endphp
+    @php $resource = [
+            'storage/inertia/js/app.js',
+            "storage/inertia/js/Pages/". $page['component'] . ".svelte",
+        ]; @endphp
     <x-inertia-head :page="$page" :resource="$resource"/>
 </head>
 <body>
--- src/Commands/Stubs/Vue/vite.config.js
+++ src/Commands/Stubs/Vue/vite.config.js
@@ -2,24 +2,26 @@ import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [
-    laravel({
-      publicDirectory: 'storage/public',
-      input: [
-        'storage/inertia/css/app.css',
-        'storage/inertia/js/app.js',
-      ],
-      ssr: 'storage/inertia/js/ssr.js',
-      refresh: true,
-    }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+  return {
+    base: isBuild ? '/public/build/' : '/',
+    plugins: [
+      laravel({
+        publicDirectory: 'storage/public',
+        input: ['storage/inertia/css/app.css', 'storage/inertia/js/app.js'],
+        ssr: 'storage/inertia/js/ssr.js',
+        refresh: true,
+
+      }),
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
         },
-      },
-    }),
-  ]
+      }),
+    ],
+  }
 });
